ea21521bf2d808d51b69f5d067f96198f548399a
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-09-12 20:48:31 +0000

    initial message

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Christina Ngwa All rights reserved .
+//
+// Created by: Christina Ngwa
+// Created on: September 2019
+// This is the "Hello, World!,", but with nice style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
